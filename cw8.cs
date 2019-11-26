d1f19db5e9c5966155aa0c773fb552fb9195b9eb
Adrian <xsodziero@gmail.com>
2019-11-26 10:42:48 +0100

    Add files via upload

---
+++ cw8.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw8
+{
+    class cw8
+    {
+        public class Prostokat
+        {
+            private double dlugosc;
+            private double szerokosc;
+            public Prostokat(double dl, double sz)
+            {
+                dlugosc = dl;
+                szerokosc = sz;
+            }
+            private double Powierzchnia()
+            {
+                return dlugosc * szerokosc;
+            }
+            private double Obwod()
+            {
+                return 2 * dlugosc + 2 * szerokosc;
+            }
+            public string Prezentuj()
+            {
+                return "Obwod: " + Obwod() + ", Powierzchnia: " + Powierzchnia();
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Prostokat p1 = new Prostokat(12, 24);
+            Console.WriteLine(p1.Prezentuj());
+            Console.ReadKey();
+        }
+    }
+}
